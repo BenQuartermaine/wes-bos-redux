1f85bac530d4feb013dd7f30f31c52da262f6513
Ben Quartermaine <ben.quartermaine3@gmail.com>
2019-03-08 22:15:49 +1100

    synutax

--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -2,7 +2,6 @@ import { createStore, compse } from 'redux';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router';
 
-// import root reducer
 import rootReducer from './reducers/index';
 
 import posts from './data/posts';
